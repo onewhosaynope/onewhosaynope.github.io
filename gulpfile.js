3e8d92c2d08331a1672f70c7905fc016370ee33e
onewhosaynope <onewhosaynope@gmail.com>
2020-07-13 14:50:09 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,25 @@ var sass = require('gulp-sass');
 var minify = require('gulp-minify');
 var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
+var del = require('del');
 
 
-//sass
-gulp.task('compile-sass', () => {
-    gulp.src(['public/scss/*.scss', 'public/scss/**/*.scss'])
+const compile_sass = () => {
+    console.log('Compiling .scss...')
+    return gulp.src(['public/scss/*.scss', 'public/scss/**/*.scss'])
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('public/gulp/css'));
-});
+}
 
 
-gulp.task('minify-css', () => {
+const minify_css = () => {
     return gulp.src('public/gulp/css/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest('public/gulp/css/minify'));
-});
+}
 
 
-gulp.task('minify-js', () => {
+const minify_js = () => {
     return gulp.src(['public/js/*.js', 'public/js/**/*.js'])
         .pipe(minify({
             ext: {
@@ -29,17 +30,32 @@ gulp.task('minify-js', () => {
             ignoreFiles: ['-min.js']
         }))
         .pipe(gulp.dest('public/gulp/js'))
-});
+}
 
 
-gulp.task('concat-js', () => {
-    gulp.src(['public/gulp/js/*.min.js', 'public/gulp/js/**/*.min.js'])
-      .pipe(concat('all.min.js'))
-      .pipe(gulp.dest('public/gulp/js/minify'))
-  });
+const concat_js = () => {
+    return gulp.src(['public/gulp/js/*.min.js', 'public/gulp/js/**/*.min.js'])
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest('public/gulp/js/minify'))
+}
 
 
-// Default task
-gulp.task('build', gulp.parallel(
-    'compile-sass', 'minify-css', 'minify-js', 'concat-js'
+const clean = () => {
+    return del('public/gulp', {force:true});
+}
+
+
+gulp.task('compile-sass', compile_sass);
+
+gulp.task('minify-css', minify_css);
+
+gulp.task('minify-js', minify_js);
+
+gulp.task('concat-js', concat_js);
+
+gulp.task('clean', clean);
+
+
+gulp.task('default', gulp.series(
+    clean, compile_sass,  minify_css, minify_js, concat_js
 ));
